c1b0c63dba22834ae037ec18dda314967db709b9
Michael Stepner <stepner@mit.edu>
2017-06-25 15:34:29 -0400

    Support spaces in filenames

--- filters/mustache.py
+++ filters/mustache.py
@@ -4,18 +4,18 @@
 Pandoc filter to apply mustache templates on regular text.
 """
 
-from pandocfilters import toJSONFilter, Str
+from pandocfilters import toJSONFilter, Str, stringify
 import pystache, yaml
 
 
 def mustache(key, value, format, meta):
     mustache_files = list_mustache_files(meta)
+    # with open('debug.txt', 'a') as the_file:
+    #     the_file.write(str(mustache_files))
+    #     the_file.write('\n')
     if key == 'Str' and mustache_files is not None:
         mustache_hashes = [yaml.load(open(file, 'r').read()) for file in mustache_files]
         mhash = { k: v for mdict in mustache_hashes for k, v in mdict.items() }
-
-        # with open('debug.txt', 'a') as the_file:
-        #     the_file.write(str(mhash))
 
         return Str(pystache.render(value, mhash))
 
@@ -27,9 +27,9 @@ def list_mustache_files(meta):
         return None
     else:
         if mustache_metadata['t'] == 'MetaInlines':
-            mustache_files = [item['c'] for item in mustache_metadata['c']]
+            mustache_files = [stringify(mustache_metadata['c'])]
         elif mustache_metadata['t'] == 'MetaList':
-            mustache_files = [item.get('c')[0].get('c') for item in mustache_metadata['c']]
+            mustache_files = [stringify(item['c']) for item in mustache_metadata['c']]
 
         return mustache_files
 
--- test/test.md
+++ test/test.md
@@ -1,7 +1,7 @@
 ---
 mustache:
     - ./test/test.yaml
-    - ./test/test2.yaml
+    - ./test with spaces/test2.yaml
 ---
 In this sentence I have {{firstnum}} and {{secondnum}}, also this word here: {{firststr}}. But {{fakevar}} isn't a real ref.
 
