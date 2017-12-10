349ccc7bf70d3e16704a2addbd15df3cbd695d82
Michael Stepner <stepner@mit.edu>
2017-12-09 23:28:43 -0500

    Switch tooling to panflute from pandocfilters
    Eliminates hacky globals that were used to avoid reparsing the mustache templates for each string. Panflute supports a "prepare"  function, which parses the metadata and creates the mustache template. Then the "action" function applies the mustache template to each string in the document.

--- filters/mustache.py
+++ filters/mustache.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python
-from pandocfilters import toJSONFilter, Str, stringify
-import pystache, yaml
-
 """
 Pandoc filter to apply mustache templates on regular text.
 """
 
-mustache_files = None
-mustache_hashes = None
-mhash = None
+from panflute import *
+import pystache, yaml
 
-def mustache(key, value, format, meta):
-    global mustache_files
-    global mustache_hashes
-    global mhash
-
-    if mustache_files is None:
-        mustache_files = list_mustache_files(meta)
-        if mustache_files is not None:
-            mustache_hashes = [yaml.load(open(file, 'r').read()) for file in mustache_files]
-            mhash = { k: v for mdict in mustache_hashes for k, v in mdict.items() }
-
+def prepare(doc):
+    """ Parse metadata to obtain list of mustache templates,
+        then load those templates.
+    """
+    doc.mustache_files = doc.get_metadata('mustache')
+    if isinstance(doc.mustache_files, basestring):  # process single YAML value stored as string
+        if not doc.mustache_files:
+            doc.mustache_files = None  # switch empty string back to None
+        else:
+            doc.mustache_files = [ doc.mustache_files ]  # put non-empty string in list
     # with open('debug.txt', 'a') as the_file:
-    #     the_file.write(str(mustache_files))
+    #     the_file.write(str(doc.mustache_files))
     #     the_file.write('\n')
-
-    if key == 'Str' and mhash is not None:
-        return Str(pystache.render(value, mhash))
-
-
-def list_mustache_files(meta):
-    mustache_metadata = meta.get("mustache", {})
-
-    if not mustache_metadata:  ## no mustache specified
-        return None
+    if doc.mustache_files is not None:
+        doc.mustache_hashes = [yaml.load(open(file, 'r').read()) for file in doc.mustache_files]
+        doc.mhash = { k: v for mdict in doc.mustache_hashes for k, v in mdict.items() }
     else:
-        if mustache_metadata['t'] == 'MetaInlines':
-            mustache_files = [stringify(mustache_metadata['c'])]
-        elif mustache_metadata['t'] == 'MetaList':
-            mustache_files = [stringify(item['c']) for item in mustache_metadata['c']]
+        doc.mhash = None
 
-        return mustache_files
+def action(elem, doc):
+    """ Apply combined mustache template to all strings in document.
+    """
+    if type(elem) == Str and doc.mhash is not None:
+        try:
+            elem.text = pystache.render(elem.text, doc.mhash)
+        except:
+            pass
+        return elem
 
-if __name__ == "__main__":
-    toJSONFilter(mustache)
+def main(doc=None):
+    return run_filter(action, prepare=prepare, doc=doc)
+
+if __name__ == '__main__':
+    main()
