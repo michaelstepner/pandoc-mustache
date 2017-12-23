a41d031b3bdc81fb133ff598d20b6f335f6bb44f
Michael Stepner <stepner@mit.edu>
2017-12-23 14:05:35 -0500

    Add initial tests
    Run from root of repo using `pytest`

---
+++ tests/test_yaml_headers.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+def test_yaml_header_styles(tmpdir):
+
+    # Prepare file names
+    doc = tmpdir.join("document.md")
+    template = tmpdir.join("template.yaml")
+
+    # Prepare file contents
+    doc_metadata = '''---
+mustache: {mustachefile}
+---
+'''
+    doc_text = 'Hello {{place}}'
+    template_content = "place: 'world'"
+    mfiles = { "mustachefile": template }
+
+    # Write contents to files
+    with open(doc, "a") as myfile:
+        myfile.write(doc_metadata.format(**mfiles))
+        myfile.write(doc_text)
+    template.write(template_content)
+
+    # Run pandoc
+    output = subprocess.check_output(["pandoc", doc, "--filter", "./filters/mustache.py"], universal_newlines=True)
+
+    # Test output
+    assert output == "<p>Hello world</p>\n"
